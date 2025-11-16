751be984d23ebdc012beb7842b1bc0afd6247dcf
Ratim <ratimboru@gmail.com>
2025-11-16 20:02:22 +0300

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
-class SearchForm(forms.Form):
-    q = forms.CharField(max_length=100, required=False)
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
