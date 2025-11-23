b6e8eee0d7f707827bd9e21704675bbb804eec69
Ratim <ratimboru@gmail.com>
2025-11-23 23:16:35 +0300

    Setting Up a New Django Project with Django REST Framework

--- advanced_features_and_security/LibraryProject/relationship_app/urls.py
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('', views.home, name='home'),
 
 ]
