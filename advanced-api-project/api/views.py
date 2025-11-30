db3ac6b61fbf4fe08f3fb76f3793973929c29b01
Ratim <ratimboru@gmail.com>
2025-11-30 23:54:13 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from datetime import datetime
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 # ListView: anyone can read
 class BookListView(generics.ListAPIView):
