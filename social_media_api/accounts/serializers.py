8419d9ed41f6d8a02a16abdbb9a1f7365fe8faaa
Ratim <ratimboru@gmail.com>
2025-12-14 15:31:40 +0300

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,10 +1,7 @@
-# social_media_api/accounts/serializers.py
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import User
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -21,34 +18,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        Token.objects.get_or_create(user=user)
+        # ✅ Use create_user
+        user = User.objects.create_user(password=password, **validated_data)
+        # ✅ Use Token.objects.create
+        Token.objects.create(user=user)
         return user
-
-class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-    token = serializers.CharField(read_only=True)
-
-    def validate(self, attrs):
-        user = authenticate(username=attrs.get("username"), password=attrs.get("password"))
-        if not user:
-            raise serializers.ValidationError("Invalid credentials.")
-        token, _ = Token.objects.get_or_create(user=user)
-        return {"token": token.key}
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    following_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = User
-        fields = ["username", "email", "bio", "profile_picture", "followers_count", "following_count"]
-
-    def get_followers_count(self, obj):
-        return obj.followers.count()
-
-    def get_following_count(self, obj):
-        return obj.following.count()
